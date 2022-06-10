868799d6b185e83f60242ba0a5d840a37e43c47a
Stephanie Conde <stephaniecondecareer@gmail.com>
2022-06-10 15:55:14 -0700

    commit tree e2b935a9e3d6bbe0d3c00e8fe163ad866e6cfc0a

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
-// Write your code here
+    const num1 = 2
+    const num2 = 31
+    const multiply = num1 * num2;
+
+    let random = Math.ceil(Math.random()*10);
+
+    let num3 = 19
+    let num4 = 5
+    let mod = num3 % num4;
+
+ 
+    let max = Math.max(10,20);
+    
+
